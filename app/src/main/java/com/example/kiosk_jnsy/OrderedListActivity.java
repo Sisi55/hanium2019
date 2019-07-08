0a3bece221f06d988c249a28c1767a39b985d4f1
sisi <dqgsh1055@gmail.com>
2019-07-08 13:27:59 +0900

    시현_프로젝트 초기화

---
+++ app/src/main/java/com/example/kiosk_jnsy/OrderedListActivity.java
@@ -0,0 +1,13 @@
+package com.example.kiosk_jnsy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderedListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordered_list);
+    }
+}

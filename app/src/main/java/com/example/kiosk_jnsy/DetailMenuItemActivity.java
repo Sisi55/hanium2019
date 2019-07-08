25fd8323fc655ae6928539201d130a9615cd2680
sisi <dqgsh1055@gmail.com>
2019-07-08 15:23:49 +0900

    시현_테스트 커밋

--- app/src/main/java/com/example/kiosk_jnsy/DetailMenuItemActivity.java
+++ app/src/main/java/com/example/kiosk_jnsy/DetailMenuItemActivity.java
@@ -1,13 +1,31 @@
 package com.example.kiosk_jnsy;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.kiosk_jnsy.databinding.ActivityDetailMenuItemBinding;
+
 public class DetailMenuItemActivity extends AppCompatActivity {
+
+
+    ActivityDetailMenuItemBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_menu_item);
-    }
-}
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_menu_item);
+
+        // 해시 키값 알면 해당 데이터 가져올 수 있나 ?
+        // 테스트
+
+
+
+
+    }//end onCreate
+
+
+
+}//end Activity

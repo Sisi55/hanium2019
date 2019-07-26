17f5e394c3d10624dc7a1ac577cf63073e9cd0fa
Beakjiyeon <bjy1530@naver.com>
2019-07-26 19:08:05 +0900

    지연 :전체주문기록 불러옴

--- change/src/main/java/com/example/change/OrderedListFragment.java
+++ change/src/main/java/com/example/change/OrderedListFragment.java
@@ -14,11 +14,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.change.model.CafeItem;
+import com.example.change.model.Order;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static java.lang.String.valueOf;
 
 
 /**
@@ -26,7 +35,8 @@ import java.util.List;
  */
 public class OrderedListFragment extends Fragment {
 
-// 시현
+    List<Order> people;
+    // 시현
     // 자기자신 - 싱글톤
     public static OrderedListFragment fragment = new OrderedListFragment();
 
@@ -37,7 +47,6 @@ public class OrderedListFragment extends Fragment {
     //end method
 
 
-
     public OrderedListFragment() {
         // Required empty public constructor
     }
@@ -46,15 +55,21 @@ public class OrderedListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        people=new ArrayList<Order>();
+        // 파이어베이스에서 읽어오기
+
+        final OrderedListFragment.OrderAdapter adapter;
+
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_ordered_list, container, false);
 
         //어댑터 만들기
-        OrderedListFragment.CafeItemAdapter adapter=new OrderedListFragment.CafeItemAdapter(new OrderedListFragment.CafeItemAdapter.OnCafeItemClickListener() {
+        adapter=new OrderedListFragment.OrderAdapter(new OrderedListFragment.OrderAdapter.OnOrderClickListener() {
             @Override
-            public void onCafeItemClicked(CafeItem model) {
+            public void onOrderClicked(Order model) {
                 // 주문기록에 있는 메뉴를 선택하면
-                Toast.makeText(getActivity(), model.getName(), Toast.LENGTH_SHORT).show();
+
                 // 상세메뉴 페이지로 넘어감
 /*
                 Intent intent = new Intent(OrderedListActivity.this, DetailMenuItemActivity.class);
@@ -70,26 +85,50 @@ public class OrderedListFragment extends Fragment {
         });
         recyclerView=(RecyclerView) view.findViewById(R.id.recyclerView);
         recyclerView.setAdapter(adapter);
-        // 실제 데이터 넣기
-        // 현재 커피공장에 있는 메뉴
-        CafeItem realmenu[]=new CafeItem[9];
-
-        List<CafeItem> people=new ArrayList<>();
 
         // 배열에 음료 데이터 넣기
-        // 1. 파이어베이스에 이 정보를 넣자
-        DatabaseReference mRootRef= FirebaseDatabase.getInstance().getReference();
-        DatabaseReference mConditionRef=mRootRef.child("condition1");
 
         // 2. 읽어와서 이 배열에 넣자
-        // 근데 메뉴판은 파베인 이유가 뭐지
-        people.add(new CafeItem("아메리카노",2000,"","aaa"));
-        people.add(new CafeItem("카페라떼",3000,"","bbb"));
-
 
         Toast.makeText(getActivity(), "음료 추가", Toast.LENGTH_SHORT).show();
+        // 파이어베이스에서 읽
+        // 데이터베이스 읽기 #2. Single ValueEventListener
+        FirebaseDatabase.getInstance().getReference().child("order").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
 
-        adapter.setItems(people);
+                    // uuid가 아래와 같은 경우만 add함////////////////////////////////////////////////////
+                    String guest = (String) snapshot.child("guest").getValue();
+                    ArrayList<CafeItem> items = (ArrayList<CafeItem>)snapshot.child("items").getValue();
+                    String today=(String)snapshot.child("today").getValue();
+
+                    // 필요 없음
+                    Map<String,Double> emotion=(Map)snapshot.child("emotion").getValue();
+                    Map<String,Double> weather=(Map)snapshot.child("weather").getValue();
+
+                    //String imageUrl=(String)snapshot.child("imageUrl").getValue();
+
+                    // 객체 형태로 받아와야 함. 오류...
+                    //Order ciObject = dataSnapshot.getValue(Order.class);
+                    people.add(new Order(emotion,weather,items,today,guest));
+                    Toast.makeText(getActivity(), "현재"+people.size(), Toast.LENGTH_SHORT).show();
+
+                }
+                // for문 다 수행 후 어댑터 설정
+                adapter.setItems(people);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        Toast.makeText(getActivity(), "종료"+people.size(), Toast.LENGTH_SHORT).show();
         Log.d("백지연","dd");
 
 
@@ -107,38 +146,38 @@ public class OrderedListFragment extends Fragment {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_menu_list);
     }
-    private static class CafeItemAdapter extends RecyclerView.Adapter<OrderedListFragment.CafeItemAdapter.CafeItemViewHolder> {
-        interface OnCafeItemClickListener {
-            void onCafeItemClicked(CafeItem model);
+    private static class OrderAdapter extends RecyclerView.Adapter<OrderedListFragment.OrderAdapter.OrderViewHolder> {
+        interface OnOrderClickListener {
+            void onOrderClicked(Order model);
         }
 
-        private OrderedListFragment.CafeItemAdapter.OnCafeItemClickListener mListener;
+        private OrderedListFragment.OrderAdapter.OnOrderClickListener mListener;
 
-        private List<CafeItem> mItems = new ArrayList<>();
+        private List<Order> mItems = new ArrayList<>();
 
-        public CafeItemAdapter() {}
+        public OrderAdapter() {}
 
-        public CafeItemAdapter(OrderedListFragment.CafeItemAdapter.OnCafeItemClickListener listener) {
+        public OrderAdapter(OrderedListFragment.OrderAdapter.OnOrderClickListener listener) {
             mListener = listener;
         }
 
-        public void setItems(List<CafeItem> items) {
+        public void setItems(List<Order> items) {
             this.mItems = items;
             notifyDataSetChanged();
         }
 
         @NonNull
         @Override
-        public OrderedListFragment.CafeItemAdapter.CafeItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public OrderedListFragment.OrderAdapter.OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.item_menu, parent, false);
-            final OrderedListFragment.CafeItemAdapter.CafeItemViewHolder viewHolder = new OrderedListFragment.CafeItemAdapter.CafeItemViewHolder(view);
+            final OrderedListFragment.OrderAdapter.OrderViewHolder viewHolder = new OrderedListFragment.OrderAdapter.OrderViewHolder(view);
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (mListener != null) {
-                        final CafeItem item = mItems.get(viewHolder.getAdapterPosition());
-                        mListener.onCafeItemClicked(item);
+                        final Order item = mItems.get(viewHolder.getAdapterPosition());
+                        mListener.onOrderClicked(item);
                     }
                 }
             });
@@ -146,12 +185,22 @@ public class OrderedListFragment extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull OrderedListFragment.CafeItemAdapter.CafeItemViewHolder holder, int position) {
-            CafeItem item = mItems.get(position);
+        public void onBindViewHolder(@NonNull OrderedListFragment.OrderAdapter.OrderViewHolder holder, int position) {
+            Order item = mItems.get(position);
             // TODO : 데이터를 뷰홀더에 표시하시오
-            holder.name.setText(item.getName());
-            holder.price.setText(item.getPrice()+"");
-            holder.img.setText(item.getImageUrl()+"");
+            holder.guest.setText(item.getGuest());
+            // arraylist에는 cafeitem 여러개가 있다
+            //
+            // 자기 주문기록에는 날짜와 메뉴만 나오면 될듯
+            String s="";
+            CafeItem ca;
+            ArrayList<CafeItem> list=item.getItems();
+            for(int i=0;i<list.size();i++){
+                Log.d("타입",(list.get(i))+"");
+            }
+
+            holder.items.setText(item.getItems().get(0)+"");
+            //holder.emotion.setText(item.getEmotion()+"");
         }
 
         @Override
@@ -159,18 +208,18 @@ public class OrderedListFragment extends Fragment {
             return mItems.size();
         }
 
-        public static class CafeItemViewHolder extends RecyclerView.ViewHolder {
+        public static class OrderViewHolder extends RecyclerView.ViewHolder {
             // TODO : 뷰홀더 완성하시오
-            TextView name;
-            TextView price;
-            TextView img;
+            TextView guest;
+            TextView items;
+            TextView emotion;
 
-            public CafeItemViewHolder(@NonNull View itemView) {
+            public OrderViewHolder(@NonNull View itemView) {
                 super(itemView);
                 // TODO : 뷰홀더 완성하시오
-                name=itemView.findViewById(R.id.name_text);
-                price=itemView.findViewById(R.id.price_text);
-                img=itemView.findViewById(R.id.img_text);
+                guest=itemView.findViewById(R.id.name_text);
+                items=itemView.findViewById(R.id.price_text);
+                //emotion=itemView.findViewById(R.id.img_text);
             }
         }//end class
     }//end class

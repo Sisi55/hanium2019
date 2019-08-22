67c60c9b67432748605410d4cfc3056f0f611826
Baekjiyeon <aa01047353119@gmail.com>
2019-08-23 00:32:45 +0900

    지연:TTS 적용.메뉴판메뉴선택시소리.상세페이지엔 설명변수생각중

--- app/src/main/java/com/example/kiosk_jnsy/MenuListActivity.java
+++ app/src/main/java/com/example/kiosk_jnsy/MenuListActivity.java
@@ -5,6 +5,8 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -46,12 +48,16 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import static java.lang.String.valueOf;
 
 // 이미지 업로드 https://www.youtube.com/watch?v=6u0gzjth4IE
 public class MenuListActivity extends AppCompatActivity {
 
+
+    // tts추가
+    private TextToSpeech mTTS;
 
     List<CafeItem> citems; // 메뉴판 객체들을 담은 arraylist
     CafeItemAdapter adapter;
@@ -131,6 +137,25 @@ public class MenuListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_list);
+
+        // tts추가
+        mTTS=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status==TextToSpeech.SUCCESS){
+                    int result=mTTS.setLanguage(Locale.GERMAN);
+                    if(result==TextToSpeech.LANG_MISSING_DATA
+                            ||result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("tts","language not supported");
+                    }
+                    else{
+                        //mButtonSpeak.setEnabled(true);
+                    }
+                }else{
+                    Log.e("tts","initialized failed");
+                }
+            }
+        });
         citems=new ArrayList<CafeItem>();
 
         // storage
@@ -147,6 +172,8 @@ public class MenuListActivity extends AppCompatActivity {
                 //Toast.makeText(MenuListActivity.this, model.getName()+" 상세정보 보기", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MenuListActivity.this, DetailMenuItemActivity.class);
                 intent.putExtra("detail",model);
+                // tts : 메뉴명 말하기
+                speak(model.getName());
                 ArrayList<CafeItem> shoplist=(ArrayList<CafeItem>) getIntent().getSerializableExtra("shoplist");
 
                 intent.putExtra("shoplist",shoplist); // 지연 : 고대로 보냄
@@ -311,4 +338,31 @@ public class MenuListActivity extends AppCompatActivity {
         }
     }//CafeItemAdapter 클래스
 
+
+    private void speak(String text){
+
+        /*
+      //  float speed=(float)mSeekBarSpeed.getProgress()/50;
+      //  if(speed<0.1) speed=0.1f;
+
+        mTTS.setPitch(pitch);
+        mTTS.setSpeechRate(speed);
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH,null);
+*/
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mTTS.speak(text,TextToSpeech.QUEUE_FLUSH,null,null);
+        } else {
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+    @Override
+    protected void onDestroy() {
+
+        if(mTTS!=null){
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+        super.onDestroy();
+    }
 }

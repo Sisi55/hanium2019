df4c73b1969214ab5c026a1c4781bd6d7b6102f9
sisi <sh20171055@duksung.ac.kr>
2019-09-22 18:09:51 +0900

    시현_선호도 주석 수정

--- app/src/main/java/com/example/kiosk_jnsy/PaymentListActivity.java
+++ app/src/main/java/com/example/kiosk_jnsy/PaymentListActivity.java
@@ -271,7 +271,7 @@ public class PaymentListActivity extends AppCompatActivity {
                 if(AppSetting.personUUID != null) {// 사용자가 얼굴인식을 하지 않는 경우를 생각한다
                     // 선호도
                     for(CafeItem item:mArrayList){
-//                        incrementPreferences(item.getName(), AppSetting.PREFERENCE_ORDER); // 선호도 +5 증가
+                        incrementPreferences(item.getName(), AppSetting.PREFERENCE_ORDER); // 선호도 +5 증가
                     }
                     // DB 갱신
                     updateDB();
